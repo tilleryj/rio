5cd57afc4a4ea0b0127081781d06bff20d1c8ab6
tilleryj <tilleryj@gmail.com>
2010-02-23 16:12:15 -0600

    making rio routes include dev/test rather than exclude production

--- lib/rio/rio_routes.rb
+++ lib/rio/rio_routes.rb
@@ -4,13 +4,13 @@ module ActionController
       alias_method :draw_without_rio_routes, :draw
       def draw
         draw_without_rio_routes do |map|
-          map.connect 'rio/fixtures', :controller => 'rio_spec', :action => 'fixtures' unless RAILS_ENV == "production"
-          map.connect 'rio/specs', :controller => 'rio_spec', :action => 'specs' unless RAILS_ENV == "production"
+          map.connect 'rio/fixtures', :controller => 'rio_spec', :action => 'fixtures' if RAILS_ENV == "development" or RAILS_ENV == "test" or ENV["rio_proxy"] == "true"
+          map.connect 'rio/specs', :controller => 'rio_spec', :action => 'specs' if RAILS_ENV == "development" or RAILS_ENV == "test" or ENV["rio_proxy"] == "true"
           map.connect 'rio/file', :controller => 'rio_file', :action => 'write' if RAILS_ENV == "development"
           map.connect 'rio/stylesheets', :controller => 'rio_proxy', :action => 'stylesheet_concat'
           yield map
           map.connect 'javascripts/*path', :controller => 'rio_proxy', :action => 'javascripts' if RAILS_ENV == "development" or RAILS_ENV == "test" or ENV["rio_proxy"] == "true"
-          map.connect 'sounds/*path', :controller => 'rio_proxy', :action => 'sounds' if RAILS_ENV == "development"
+          map.connect 'sounds/*path', :controller => 'rio_proxy', :action => 'sounds' if RAILS_ENV == "development" or RAILS_ENV == "test" or ENV["rio_proxy"] == "true"
           map.connect 'images/*path', :controller => 'rio_proxy', :action => 'images'
           map.connect 'stylesheets/*path', :controller => 'rio_proxy', :action => 'stylesheets'
           map.connect 'push/subscribe', :controller => 'rio_push', :action => 'subscribe'
